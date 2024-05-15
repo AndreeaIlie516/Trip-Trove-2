7556c9e91004592bc8303404dfbb2c1bc85971bf
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2024-05-15 15:50:20 +0300

    User Entity & authentification added

--- Trip-Trove-API/main.go
+++ Trip-Trove-API/main.go
@@ -27,6 +27,7 @@ func main() {
 	entitiesToMigrate := []interface{}{
 		&entities.Destination{},
 		&entities.Location{},
+		&entities.User{},
 	}
 
 	for _, entity := range entitiesToMigrate {
@@ -40,17 +41,23 @@ func main() {
 
 	go websocketManager.BroadcastWebSocketMessage()
 
+	authMiddleware := middlewares.AuthMiddleware{}
+
 	destinationRepository := dataaccess.NewGormDestinationRepository(db)
 	locationRepository := dataaccess.NewGormLocationRepository(db)
+	userRepository := dataaccess.NewGormUserRepository(db)
 
 	destinationService := services.DestinationService{Repo: destinationRepository, LocationRepo: locationRepository, WsManager: websocketManager}
 	locationService := services.LocationService{Repo: locationRepository, DestinationRepo: destinationRepository}
+	userService := services.UserService{Repo: userRepository}
 
 	destinationHandler := handlers.DestinationHandler{Service: &destinationService}
 	locationHandler := handlers.LocationHandler{Service: &locationService}
+	userHandler := handlers.UserHandler{Service: &userService}
 
 	routes.RegisterDestinationRoutes(router, &destinationHandler)
 	routes.RegisterLocationRoutes(router, &locationHandler)
+	routes.RegisterUserRoutes(router, &userHandler, authMiddleware)
 
 	wsController := handlers.WebSocketHandler{Service: &destinationService, WebSocketManager: websocketManager}
 	routes.RegisterWebSocketRoutes(router, &wsController)
--- Trip-Trove-API/utils/validators.go
+++ Trip-Trove-API/utils/validators.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"github.com/go-playground/validator/v10"
+	"strings"
 	"unicode"
 )
 
@@ -61,4 +62,57 @@ func DescriptionValidator(fl validator.FieldLevel) bool {
 	}
 
 	return true
+}
+
+func UsernameValidator(fl validator.FieldLevel) bool {
+	username := fl.Field().String()
+
+	if len(username) < 3 || len(username) > 40 {
+		return false
+	}
+
+	for _, char := range username {
+		if !(unicode.IsLetter(char) || unicode.IsDigit(char) || char == '_' || char == '-' || char == '.') {
+			return false
+		}
+	}
+
+	return true
+}
+
+func PasswordValidator(fl validator.FieldLevel) bool {
+	if password, ok := fl.Field().Interface().(string); ok {
+		return isComplexPassword(password)
+	}
+	return false
+}
+
+func isComplexPassword(password string) bool {
+	var (
+		hasMinLen  = len(password) >= 8
+		hasMaxLen  = len(password) <= 40
+		hasUpper   = false
+		hasLower   = false
+		hasNumber  = false
+		hasSpecial = false
+	)
+
+	for _, char := range password {
+		print(char)
+		switch {
+		case 'a' <= char && char <= 'z':
+			hasLower = true
+		case 'A' <= char && char <= 'Z':
+			hasUpper = true
+		case '0' <= char && char <= '9':
+			hasNumber = true
+		case strings.ContainsRune("@$!%*?&", char):
+
+			print("Special")
+			hasSpecial = true
+		}
+	}
+	print(hasMinLen, hasMaxLen, hasUpper, hasLower, hasNumber, hasSpecial)
+
+	return hasMinLen && hasMaxLen && hasUpper && hasLower && hasNumber && hasSpecial
 }
